e21c01b40baf0554b8b7c70f02a8d9a2eea21fc1
zjuwang265 <2295073478@QQ.com>
2024-07-07 22:36:53 +0800

    Update algebra.c

--- src/algebra.c
+++ src/algebra.c
@@ -38,44 +38,101 @@ Matrix sub_matrix(Matrix a, Matrix b)
 
 Matrix mul_matrix(Matrix a, Matrix b)
 {
-    // ToDo
+     if (a.cols != b.rows) {
+        fprintf(stderr, "Matrix dimensions do not conform for multiplication.\n");
+        exit(EXIT_FAILURE);
+    }
+    Matrix result = create_matrix(a.rows, b.cols);
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < b.cols; j++) {
+            for (int k = 0; k < a.cols; k++) {
+                result.data[i][j] += a.data[i][k] * b.data[k][j];
+            }
+        }
+    }
     return create_matrix(0, 0);
 }
 
 Matrix scale_matrix(Matrix a, double k)
 {
-    // ToDo
+     Matrix result = create_matrix(a.rows, a.cols);
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < a.cols; j++) {
+            result.data[i][j] = a.data[i][j] * k;
+        }
+    }
     return create_matrix(0, 0);
 }
 
 Matrix transpose_matrix(Matrix a)
 {
-    // ToDo
+     Matrix result = create_matrix(a.cols, a.rows);
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < a.cols; j++) {
+            result.data[j][i] = a.data[i][j];
+        }
+    }
     return create_matrix(0, 0);
 }
 
 double det_matrix(Matrix a)
 {
-    // ToDo
-    return 0;
+     if (a.rows != 2 || a.cols != 2) {
+        fprintf(stderr, "Determinant calculation only supported for 2x2 matrices.\n");
+        exit(EXIT_FAILURE);
+    }
+    return a.data[0][0] * a.data[1][1] - a.data[0][1] * a.data[1][0];
 }
+
 
 Matrix inv_matrix(Matrix a)
 {
-    // ToDo
+    if (a.rows != 2 || a.cols != 2) {
+        fprintf(stderr, "Inverse matrix calculation only supported for 2x2 matrices.\n");
+        exit(EXIT_FAILURE);
+    }
+    double det = det_matrix(a);
+    if (det == 0) {
+        fprintf(stderr, "Matrix is not invertible.\n");
+        exit(EXIT_FAILURE);
+    }
+    Matrix result = create_matrix(2, 2);
+    result.data[0][0] = a.data[1][1] / det;
+    result.data[0][1] = -a.data[0][1] / det;
+    result.data[1][0] = -a.data[1][0] / det;
+    result.data[1][1] = a.data[0][0] / det;
     return create_matrix(0, 0);
 }
 
 int rank_matrix(Matrix a)
 {
-    // ToDo
-    return 0;
+     if (a.rows != a.cols) {
+        fprintf(stderr, "Rank calculation only supported for square matrices.\n");
+        exit(EXIT_FAILURE);
+    }
+    int rank = 0;
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < a.cols; j++) {
+            if (a.data[i][j] != 0) {
+                rank++;
+                break;
+            }
+        }
+    }
+    return rank;
 }
 
 double trace_matrix(Matrix a)
 {
-    // ToDo
-    return 0;
+      if (a.rows != a.cols) {
+        fprintf(stderr, "Trace calculation only supported for square matrices.\n");
+        exit(EXIT_FAILURE);
+    }
+    double trace = 0;
+    for (int i = 0; i < a.rows; i++) {
+        trace += a.data[i][i];
+    }
+    return trace;
 }
 
 void print_matrix(Matrix a)
